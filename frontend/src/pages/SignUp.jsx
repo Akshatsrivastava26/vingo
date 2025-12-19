71357e35dcab4bb28058f62c91217fdeb8220ee7
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-19 16:47:17 +0530

    feat: add password visibility toggle in SignUp component

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useState } from 'react';
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
 
 function SignUp() {
   const primaryColor = "#ff4d2d";
   const hoverColor = "#e64323";
   const bgColor = "#fff9f9";
   const borderColor = "#ddd";
-  
 
+const [showPassword, setShowPassword] = useState(false);
   return (
     <div className='min-h-screen w-full flex items-center justify-center p-4'style={{background:bgColor}}>
       <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px]`} style={{border:`1px solid${borderColor}>`}}>
@@ -29,6 +32,17 @@ function SignUp() {
         <div className='mb-4'>
           <label htmlFor="mobile" className='block text-gray-700 font-medium mb-1'>Mobile Number</label>
           <input type="mobile" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Mobile Number' style={{border: `1px solid ${borderColor}`}} />
+        </div>
+
+        {/* password */}
+        <div className='mb-4'>
+          <label htmlFor="password" className='block text-gray-700 font-medium mb-1'>Password</label>
+          <div className='relative'>
+            <input type="password" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Password' style={{border: `1px solid ${borderColor}`}} />
+            <button className='absolute right-3 cursor-pointer top-[14px] text-gray-500' onClick={()=>setShowPassword(prev=>!prev)}>{!showPassword?<FaRegEye />:<FaRegEyeSlash />}</button>
+          </div>
+          
+          
         </div>
         
 
