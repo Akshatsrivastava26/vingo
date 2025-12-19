0b1c9a72f92b03fe1b8aae4b9baa80f4f09faefc
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-19 18:22:43 +0530

    feat: add axios dependency and enhance SignUp component with role selection and Google sign-up option

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 function SignUp() {
   const primaryColor = "#ff4d2d";
   const hoverColor = "#e64323";
   const bgColor = "#fff9f9";
   const borderColor = "#ddd";
-
-const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [role, setRole] = useState("user");
+  const navigate=useNavigate();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className='min-h-screen w-full flex items-center justify-center p-4'style={{background:bgColor}}>
       <div className={`bg-white rounded-xl shadow-lg w-full max-w-md p-8 border-[1px]`} style={{border:`1px solid${borderColor}>`}}>
@@ -19,33 +26,52 @@ const [showPassword, setShowPassword] = useState(false);
         {/* fullName */}
         <div className='mb-4'>
           <label htmlFor="fullName" className='block text-gray-700 font-medium mb-1'>Full Name</label>
-          <input type="text" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Full Name' style={{border: `1px solid ${borderColor}`}} />
+          <input type="text" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Full Name' style={{border: `1px solid ${borderColor}`}} onClick={(e)=>setFullName(e.target.value)} value={fullName}/>
         </div>
 
         {/* email */}
         <div className='mb-4'>
           <label htmlFor="email" className='block text-gray-700 font-medium mb-1'>Email</label>
-          <input type="email" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Email' style={{border: `1px solid ${borderColor}`}} />
+          <input type="email" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Email' style={{border: `1px solid ${borderColor}`}} onClick={(e)=>setEmail(e.target.value)} value={email}/>
         </div>
 
         {/* mobile */}
         <div className='mb-4'>
           <label htmlFor="mobile" className='block text-gray-700 font-medium mb-1'>Mobile Number</label>
-          <input type="mobile" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Mobile Number' style={{border: `1px solid ${borderColor}`}} />
+          <input type="mobile" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Mobile Number' style={{border: `1px solid ${borderColor}`}} onClick={(e)=>setMobile(e.target.value)} value={mobile}/>
         </div>
 
         {/* password */}
         <div className='mb-4'>
           <label htmlFor="password" className='block text-gray-700 font-medium mb-1'>Password</label>
           <div className='relative'>
-            <input type="password" className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Password' style={{border: `1px solid ${borderColor}`}} />
+            <input type={`${showPassword ? "text" : "password"}`} className='w-full border rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Password' style={{border: `1px solid ${borderColor}`}} onClick={(e)=>setPassword(e.target.value)} value={password}/>
             <button className='absolute right-3 cursor-pointer top-[14px] text-gray-500' onClick={()=>setShowPassword(prev=>!prev)}>{!showPassword?<FaRegEye />:<FaRegEyeSlash />}</button>
           </div>
-          
-          
         </div>
-        
 
+        {/* role */}
+        <div className='mb-4'>
+          <label htmlFor="role" className='block text-gray-700 font-medium mb-1'>Role</label>
+          <div className='flex gap-2'>
+            {['user','owner','deliveryBoy'].map((r)=>(
+              <button className='flex-1 border rounded-lg px-3 py-2 text-center font-medium transition-colors cursor-pointer'onClick={()=>setRole(r)} 
+              style={
+                role==r?
+                {backgroundColor:primaryColor,color:'white'}
+                :{border:`1px solid ${primaryColor}`,color:primaryColor,}
+              }>{r}</button>
+            
+            ))}
+          </div>
+        </div>
+        <button className={`w-full  mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer`}>Sign Up</button>
+
+        <button className='w-full mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 border-gray-400 hover:bg-gray-100 cursor-pointer'>
+          <FcGoogle size={20}/>
+          <span>Sign Up with Google</span>
+          </button>
+          <p className='text-center mt-6 cursor-pointer'onClick={()=>navigate("/signin")}>Already have an account? <span className='text-[#ff4d2d]'>Sign In</span></p>
       </div>
     </div>
   )
