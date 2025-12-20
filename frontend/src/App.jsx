ee3ecc2432c95196b96879802bdac3d32ca97c0b
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-20 15:51:31 +0530

    fix: add missing semicolon in serverUrl declaration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import ForgotPassword from './pages/ForgotPassword'
-export const serverUrl="http://localhost:8000"
+export const serverUrl="http://localhost:8000";
 
 function App() {
   return (
