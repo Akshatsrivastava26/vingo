4cd767fb1353d954dce82047203bc10e15261a5c
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-22 15:00:21 +0530

    feat: implement ForgotPassword component with email and OTP input steps

--- frontend/src/pages/ForgotPassword.jsx
+++ frontend/src/pages/ForgotPassword.jsx
@@ -1,14 +1,37 @@
 import React, {useState} from 'react';
 import { IoIosArrowRoundBack } from "react-icons/io";
+import { useNavigate } from 'react-router-dom';
 function ForgotPassword() {
   const [step, setStep] = React.useState(1);
+  const [email, setEmail] = useState("");
+  const navigate=useNavigate();
   return (
     <div className='flex w-full items-center justify-center min-h-screen p-4 bg-[#fff9f6]'>
       <div className='bg-white rounded-xl shadow-lg w-full max-w-md p-8'>
         <div className='flex items-center gap-4 mb-4'>
-          <IoIosArrowRoundBack size={30} className='text-[#ff4d2d]'/>
+          <IoIosArrowRoundBack size={30} className='text-[#ff4d2d] cursor-pointer' onClick={() => navigate("/signin")} />
          <h1 className='text-3xl font-bold text-center text-[#ff4d2d]'>Forgot Password</h1>
         </div>
+        {
+        step === 1 && 
+         <div>
+          {/* email */}
+        <div className='mb-6'>
+          <label htmlFor="email" className='block text-gray-700 font-medium mb-1'>Email</label>
+          <input type="email" className='w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Email' onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        </div>
+        <button className="w-full  mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer">Send Otp</button>
+         </div>}
+
+         {step === 2 && 
+         <div>
+          {/* Otp */}
+        <div className='mb-6'>
+          <label htmlFor="otp" className='block text-gray-700 font-medium mb-1'>Otp</label>
+          <input type="text" className='w-full border-[1px] border-gray-200 rounded-lg px-3 py-2 focus:outline-none focus:border-orange-500' placeholder='Enter your Otp' onChange={(e)=>setEmail(e.target.value)} value={email}/>
+        </div>
+        <button className="w-full  mt-4 flex items-center justify-center gap-2 border rounded-lg px-4 py-2 transition duration-200 bg-[#ff4d2d] text-white hover:bg-[#e64323] cursor-pointer">Send Otp</button>
+         </div>}
 
       </div>
     </div>
