497b8b9aaebd445bb0a79cfacabdb8e8ac3377d2
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-27 21:09:43 +0530

    feat: refactor useGetCity hook to utilize user data and improve geolocation city fetching

--- frontend/src/hooks/useGetCity.jsx
+++ frontend/src/hooks/useGetCity.jsx
@@ -3,17 +3,20 @@ import { useEffect } from 'react'
 import axios from 'axios';
 import { serverUrl } from '../App.jsx';
 import { useDispatch } from 'react-redux';
-import { setUserData } from '../redux/userSlice.js';
+import { setCity, setUserData } from '../redux/userSlice.js';
+import { useSelector } from 'react-redux';
 
 function useGetCity() {
     const dispatch=useDispatch();
+    const {userData}=useSelector((state)=>state.user);
+    const apiKey=import.meta.env.VITE_GEO_API_KEY;
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(async (position) => {
             const { latitude, longitude } = position.coords;
-            console.log(position);
-            const result = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=YOUR_API_KEY`)
+            const result = await axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&format=json&apiKey=${apiKey}`);
+            dispatch(setCity(result?.data?.results[0].city));
     });
-    }, []);
+    }, [userData]);
 }
 
 export default useGetCity
