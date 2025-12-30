a9e1a922ea15606230db5f7a56928ae34231d63c
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2025-12-30 17:41:51 +0530

    feat: clean up user routes and update Nav component for responsive search display

--- backend/routes/userroutes.js
+++ backend/routes/userroutes.js
@@ -1,8 +1,6 @@
 import express from "express";
 import { getCurrentUser } from "../controllers/user.controllers.js";
 import isAuth from "../middleware/isAuth.js";
-
-
 const userRouter = express.Router();
 
 userRouter.get("/current-user",isAuth,getCurrentUser);
