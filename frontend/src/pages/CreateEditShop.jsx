05bcca7b5f4b95dadc959095ceb39207c19787c2
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2026-01-03 17:09:23 +0530

    feat: enhance Create/Edit Shop functionality with image upload and form submission

--- frontend/src/pages/CreateEditShop.jsx
+++ frontend/src/pages/CreateEditShop.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { FaUtensils } from "react-icons/fa";
 import { useState } from 'react';
+import { serverUrl } from '../App';
+import axios from 'axios';
+import { setMyShopData } from '../redux/ownerSlice';
 function CreateEditShop() {
     const navigate = useNavigate();
     const {myShopData}=useSelector((state)=>state.owner);
@@ -12,6 +15,33 @@ function CreateEditShop() {
     const[address,setAddress]=useState(myShopData?.address || currentAddress);
     const[city,setCity]=useState(myShopData?.city || currentCity || "");
     const[state,setState]=useState(myShopData?.state || currentState );
+    const [frontendImage,setFrontendImage]=useState(myShopData?.image||null);
+    const [backendImage,setBackendImage]=useState(null);
+    const dispatch=useDispatch
+    const handelImage=(e)=>{
+        const file=e.target.files[0];
+        setBackendImage(file);
+        setFrontendImage(URL.createObjectURL(file));
+    }
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try {
+            const formData=new FormData();
+            formData.append("name",name);
+            formData.append("address",address);
+            formData.append("city",city);
+            formData.append("state",state);
+            if(backendImage){
+                formData.append("image",backendImage);
+            }
+            const result= await axios.post(`${serverUrl}/api/shop/create-edit`, formData, {withCredentials:true})
+            dispatch(setMyShopData(result.data))
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
   return (
     <div className='flex justify-center flex-col items-center p-6 bg-gradient-to-br from-orange-50 relative to-white min-h-screen'>
       <div className='absolute top-[20px] left-[20px] z-[10] mb-[10px]' onClick={()=> navigate("/")}>
@@ -28,14 +58,18 @@ function CreateEditShop() {
 
         </div>
         {/* Form Starts Here */}
-        <form className='space-y-5'>
+        <form className='space-y-5 ' onSubmit={handleSubmit}>
             <div>
                 <label className='block text-sm font-medium text-gray-700 mb-1'>Name</label>
                 <input type="text" placeholder='Enter Shop Name' className='w-full px-4 py-2 vorder rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 ' onChange={(e)=>setName(e.target.value)} value={name}/>
             </div>
             <div>
                 <label className='block text-sm font-medium text-gray-700 mb-1'>Shop Image</label>
-                <input type="file" accept='image/*' className='w-full px-4 py-2 vorder rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 ' />
+                <input type="file" accept='image/*' className='w-full px-4 py-2 vorder rounded-lg focus:outline-none focus:ring-2 focus:ring-orange-500 ' onChange={handelImage} />
+                {frontendImage && <div className='mt-4'>
+                    <img src={frontendImage} alt=""  className='w-full h-48 object-cover rounded-lg border'/>
+                </div>}
+                
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                 <div>
