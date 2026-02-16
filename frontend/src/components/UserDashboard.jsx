9ae62c6de89edd7bcd836eb58815895649c49a96
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2026-02-17 03:25:06 +0530

    feat: refactor CategoryCard props and enhance UserDashboard with shop scrolling functionality

--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -9,11 +9,16 @@ import { useSelector } from 'react-redux';
 
 
 
+
 function UserDashboard() {
-  const {currentCity}=useSelector(state=>state.user)
+  const {currentCity,shopInMyCity }=useSelector(state=>state.user)
   const cateScrollRef = React.useRef();
+  const shopScrollRef = React.useRef();
   const [showLeftCateButton, setShowLeftCateButton] = useState(false);
   const [showRightCateButton, setShowRightCateButton] = useState(false);
+  const [showLeftShopButton, setShowLeftShopButton] = useState(false);
+  const [showRightShopButton, setShowRightShopButton] = useState(false);
+  
 
   const updateButton=(ref, setLeftButton, setRightButton)=>{
     const element = ref.current;
@@ -34,14 +39,23 @@ function UserDashboard() {
   useEffect(()=>{
     if(cateScrollRef.current){
       updateButton(cateScrollRef, setShowLeftCateButton, setShowRightCateButton)
+      updateButton(shopScrollRef, setShowLeftShopButton, setShowRightShopButton)
       cateScrollRef.current.addEventListener("scroll",()=>{
         updateButton(cateScrollRef, setShowLeftCateButton, setShowRightCateButton)
+        updateButton(shopScrollRef, setShowLeftShopButton, setShowRightShopButton)
+      })
+      shopScrollRef.current.addEventListener("scroll",()=>{
+        updateButton(shopScrollRef, setShowLeftShopButton, setShowRightShopButton)
       })
     }
 
-    return ()=>cateScrollRef.current.removeEventListener("scroll",()=>{
+    return ()=>{cateScrollRef.current.removeEventListener("scroll",()=>{
         updateButton(cateScrollRef, setShowLeftCateButton, setShowRightCateButton)
       })
+      shopScrollRef.current.removeEventListener("scroll",()=>{
+        updateButton(shopScrollRef, setShowLeftShopButton, setShowRightShopButton)
+      })}
+      
   },[categories])
 
   return (
@@ -60,7 +74,7 @@ function UserDashboard() {
           
           <div className='w-full flex overflow-x-auto gap-4 pb-2 ' ref={cateScrollRef}>
             {categories.map((cate, index)=>(
-              <CategoryCard data={cate} key={index} />
+              <CategoryCard name={cate.category} image={cate.image} key={index} />
             ))}
           </div>
           {showRightCateButton && <button className='absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 ' onClick={()=>scrollHandler(cateScrollRef,"right")}>
@@ -73,7 +87,27 @@ function UserDashboard() {
       {/* Shop section */}
       <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]'>
         <h1 className='text-gray-800 text-2xl sm:text-3xl'>Best Shop in  {currentCity}</h1>
+        <div className='w-full relative'>
+          {showLeftShopButton && <button className='absolute left-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 ' onClick={()=>scrollHandler(shopScrollRef,"left")}>
+            <FaCircleChevronLeft />
+          </button>}
+          
+          <div className='w-full flex overflow-x-auto gap-4 pb-2 ' ref={shopScrollRef}>
+            {shopInMyCity?.map((shop, index)=>(
+              <CategoryCard name={shop.name} image={shop.image} key={index} />
+            ))}
+          </div>
+          {showRightShopButton && <button className='absolute right-0 top-1/2 -translate-y-1/2 bg-[#ff4d2d] text-white p-2 rounded-full shadow-lg hover:bg-[#e64528] z-10 ' onClick={()=>scrollHandler(shopScrollRef,"right")}>
+            <FaChevronCircleRight />
+          </button> }
+          
+        </div>
 
+      </div>
+
+      {/* Product section */}
+      <div className='w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]'>
+        <h1 className='text-gray-800 text-2xl sm:text-3xl'>Suggested Food Items</h1>
       </div>
 
     </div>
